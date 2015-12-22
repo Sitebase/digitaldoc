d1c26cdde3c5569fae880bbe6dd78063c6e336cc
Wim <wim@sitebase.be>
2015-12-22 11:06:55 +0100

    clean up code

--- src/index.php
+++ src/index.php
@@ -1,4 +1,6 @@
 <?php
+
+// load dependencies
 require_once __DIR__.'/../vendor/autoload.php';
 require_once 'config.php';
 require_once 'functions.php';
@@ -10,8 +12,16 @@ use GuzzleHttp\Client;
 use mikehaertl\wkhtmlto\Pdf;
 use Ramsey\Uuid\Uuid;
 
+// create silex application
 $app = new Silex\Application();
+
+// load composer file so I can use it to extract information
 $project = json_decode(file_get_contents(__DIR__.'/../composer.json'));
+
+/**
+ * Information endpoint
+ * This endpoint just returns some general information about the API
+ */
 $app->get('/api/', function() use ($project) {
     return new JsonResponse([
         'name' => $project->name,
@@ -34,6 +44,12 @@ $app->get('/api/generate/', function() {
     }
 });
 
+/**
+ * Generate PDF
+ * Send some HTML source to this endpoint and it will convert it to
+ * a PDF file
+ * It returns a unique ID as result
+ */
 $app->post('/api/test/', function(Request $request) {
 
     // save html source to local document
@@ -56,9 +72,15 @@ $app->post('/api/test/', function(Request $request) {
     }
 });
 
+/**
+ * Download PDF
+ * Use the ID that was returned in the `generate` endpoint to download the file
+ * using this endpoint
+ */
 $app->get('/api/download/{id}', function($id) {
     $file = getFile($id, 'pdf');
 
+    // make sure the file exists
     if(!file_exists($file))
         return error("file with is '$id' is not found");
 
